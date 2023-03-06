74e6c72306342a9747a6f7cad34312301be7b1dd
redhadefinto <redhadefinto28@gmail.com>
2023-03-06 23:57:43 +0700

    vercel deployment

--- src/configs/environment.js
+++ src/configs/environment.js
@@ -1,8 +1,8 @@
 module.exports = {
-  host: process.env.HOST,
+  host: process.env.DB_HOST,
   db: process.env.DB,
   dbPort: process.env.DB_PORT,
-  user: process.env.UNAME,
-  pwd: process.env.PWD,
-  serverPort: process.env.SERVER_PORT
+  user: process.env.DB_USER,
+  pwd: process.env.DB_PWD,
+  serverPort: process.env.DB_SERVER_PORT
 }
