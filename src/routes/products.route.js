a14412740136febe725ecc27e8ed77c0f1aa0c7d
redhadefinto <redhadefinto28@gmail.com>
2023-03-07 11:37:07 +0700

    pacth update

--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
-const productsRouter = Router()
-const productController = require('../controllers/product.controller')
+const productsRouter = Router();
+const productController = require('../controllers/product.controller');
 
-productsRouter.get("/", productController.getProducts)
+productsRouter.get("/", productController.getProducts);
 // product/1
-productsRouter.get("/:productId", productController.getProductDetail)
-productsRouter.post("/", productController.insertProduct)
-productsRouter.put("/:productId", productController.updateProduct)
-productsRouter.delete('/:productId', productController.deleteProduct)
+productsRouter.get("/:productId", productController.getProductDetail);
+productsRouter.post("/", productController.insertProduct);
+productsRouter.patch("/:productId", productController.updateProduct);
+productsRouter.delete('/:productId', productController.deleteProduct);
 
 module.exports = productsRouter;
