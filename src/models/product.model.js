4572e896f392f5d9e57ce3d1834e949a782a14ed
redhadefinto <redhadefinto28@gmail.com>
2023-03-07 10:39:01 +0700

    fix image

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -2,7 +2,7 @@ const db = require("../configs/postgre");
 
 const getProducts = (query) => {
   return new Promise((resolve, reject) => {
-    let sqlQuery = `select p.id, p."product_name", p.price, pc."category_name" from products p 
+    let sqlQuery = `select p.id, p."product_name", p.image, p.price, pc."category_name" from products p 
     join prod_categories pc on p.category_id = pc.id `;
     // Order by price
     let order = "p.id ASC";
@@ -41,7 +41,7 @@ const getProducts = (query) => {
 
 const getProductDetail = (params) => {
   return new Promise((resolve, reject) => {
-    const sqlQuery = `select p.id, p."product_name", p.price, pc."category_name" from products p 
+    const sqlQuery = `select p.id, p."product_name", p.image, p.price, pc."category_name" from products p 
     join prod_categories pc on p.category_id = pc.id WHERE p.id = $1`;
     const values = [params.productId];
     db.query(sqlQuery, values, (err, result) => {
@@ -56,9 +56,9 @@ const getProductDetail = (params) => {
 
 const insertProduct = (data) => {
   return new Promise((resolve, reject) => { 
-    const sqlQuery = `insert into products (product_name, price, category_id) values ($1, $2, $3) RETURNING *`;
+    const sqlQuery = `insert into products (product_name, price, category_id, size_id, delivery_method, image) values ($1, $2, $3, $4, $5, $6,) RETURNING *`;
     // parameterized query
-    const values = [data.product_name, data.price, data.category_id];
+    const values = [data.product_name, data.price, data.category_id, data.size_id, data.delivery_method, data.image];
     db.query(sqlQuery, values, (err, result) => {
       if(err) {
         reject (err);
@@ -71,8 +71,8 @@ const insertProduct = (data) => {
 
 const updateProduct = (params, data) => {
   return new Promise((resolve, reject) => {
-    const sqlQuery = `update products set product_name = $1, price = $2, category_id = $3 where id = $4 RETURNING *;`;
-    const values = [data.product_name, data.price, data.category_id, params.productId];
+    const sqlQuery = `update products set product_name = $1, price = $2, category_id = $3, size_id = $4, delivery_method = $5, image = $6 where id = $7 RETURNING *;`;
+    const values = [data.product_name, data.price, data.category_id, data.size_id, data.delivery_method, data.image, params.productId];
     db.query(sqlQuery, values, (err, result) => {
       if (err) {
         reject(err);
